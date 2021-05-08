288d85ffff25ea6c74455fac9073667af5b228bb
francosbenitez <francosbenitez@gmail.com>
2021-05-08 00:03:59 -0300

    Update script.py

--- script.py
+++ script.py
@@ -11,52 +11,6 @@ pos = soup.find(id = "promedios")
 eq = pos.find_all("tr", class_ = "ipr")
 eq += pos.find_all("tr", class_ = "pr")
 
-# teams name
-equipos = []
-nombres = []
-for equipo in eq:
-    equipo = equipo.find_all("td")
-    equipos.append(equipo)
-    for equipo in equipos:
-        nombres.append(equipo[1].text)
-
-nombres = pd.unique(pd.Series(nombres))
-
-# teams T20
-equipos = []
-T20 = []
-for equipo in eq:
-    equipo = equipo.find_all("td")
-    equipos.append(equipo)
-    for equipo in equipos:
-        T20.append(equipo[2].text)
-
-T20 = pd.unique(pd.Series(T20))
-
-# teams T21
-equipos = []
-T21 = []
-for equipo in eq:
-    equipo = equipo.find_all("td")
-    equipos.append(equipo)
-    for equipo in equipos:
-        T21.append(equipo[3].text)
-
-T21 = pd.unique(pd.Series(T21))
-
-# teams pts
-equipos = []
-pts = []
-for equipo in eq:
-    equipo = equipo.find_all("td")
-    equipos.append(equipo)
-    for equipo in equipos:
-        pts.append(equipo[4].text)
-
-pts = pd.unique(pd.Series(pts))
-
-df = pd.DataFrame({"T20": T20, "PTS": pts})
-
 def extraer_datos(columna):
     equipos = []
     lista = []
@@ -68,8 +22,60 @@ def extraer_datos(columna):
     lista = pd.unique(pd.Series(lista))
     return lista
 
-print(extraer_datos(1))
-    
+equipos = extraer_datos(1)
+T20 = extraer_datos(2)
+T21 = extraer_datos(3)
+Pts = extraer_datos(4)
+PJ = extraer_datos(5)
+Prom = extraer_datos(6)    
+
+# # teams name
+# equipos = []
+# nombres = []
+# for equipo in eq:
+#     equipo = equipo.find_all("td")
+#     equipos.append(equipo)
+#     for equipo in equipos:
+#         nombres.append(equipo[1].text)
+
+# nombres = pd.unique(pd.Series(nombres))
+
+# # teams T20
+# equipos = []
+# T20 = []
+# for equipo in eq:
+#     equipo = equipo.find_all("td")
+#     equipos.append(equipo)
+#     for equipo in equipos:
+#         T20.append(equipo[2].text)
+
+# T20 = pd.unique(pd.Series(T20))
+
+# # teams T21
+# equipos = []
+# T21 = []
+# for equipo in eq:
+#     equipo = equipo.find_all("td")
+#     equipos.append(equipo)
+#     for equipo in equipos:
+#         T21.append(equipo[3].text)
+
+# T21 = pd.unique(pd.Series(T21))
+
+# # teams pts
+# equipos = []
+# pts = []
+# for equipo in eq:
+#     equipo = equipo.find_all("td")
+#     equipos.append(equipo)
+#     for equipo in equipos:
+#         pts.append(equipo[4].text)
+
+# pts = pd.unique(pd.Series(pts))
+
+# df = pd.DataFrame({"T20": T20, "PTS": pts})
+
+
     
 
     
