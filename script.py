fdf1ae4e1112f61d2b60c93c0faa622396bccc05
francosbenitez <francosbenitez@gmail.com>
2021-06-03 19:19:22 -0300

    improve script.py

--- script.py
+++ script.py
@@ -3,11 +3,14 @@ import requests                # load html
 import pandas as pd            # data manipulation
 
 # function to extract different tables
-def extract_table(table_type):
+def extract_table(table_type, class_type=None):
     url = "https://www.promiedos.com.ar/primera"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
-    pos = soup.find(id = table_type)
+    if class_type:
+        pos = soup.find(id = table_type, class_= class_type)
+    else:
+        pos = soup.find(id = table_type)
     teams = [list(map(lambda data: data.text, row.find_all("td"))) for row in pos.find_all("tr")]
     headers = next(iter([list(map(lambda data: data.text, row.find_all("th"))) for row in pos.find_all("tr")]))
     df = pd.DataFrame(teams)
@@ -15,14 +18,16 @@ def extract_table(table_type):
     return df
 
 # create dataframe tables
+posiciones_zona_1 = extract_table("posiciones", class_type="tablesorter1")
+posiciones_zona_2 = extract_table("posiciones", class_type="tablesorter2")
 promedios = extract_table("promedios")
-posiciones = extract_table("tablaseccion")
+tabla_anual_2021 = extract_table("posiciones", class_type="tablesorter3")
 goleadores = extract_table("goleadorest")
 
 # save dataframe tables
-promedios.to_csv("promedios.csv")
-posiciones.to_csv("posiciones.csv")
-goleadores.to_csv("goleadores.csv")
+# promedios.to_csv("promedios.csv")
+# posiciones.to_csv("posiciones.csv")
+# goleadores.to_csv("goleadores.csv")
 
     
   
