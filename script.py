a3bc72198d9b3713ac1dfecb8b20aef7a074a6cf
francosbenitez <francosbenitez@gmail.com>
2021-06-03 10:41:12 -0300

    improve web-scraping

--- script.py
+++ script.py
@@ -8,64 +8,31 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 # specific HTML to extract
 pos = soup.find(id = "promedios")
-eq = pos.find_all("tr", class_ = "ipr")
-eq += pos.find_all("tr", class_ = "pr")
+table = pos.find_all("tr", class_ = "ipr")
+table += pos.find_all("tr", class_ = "pr")
 
-# initialize lists
-nameArray = []
-name = []
-statsArray = []
-T20 = []
-T21 = []
-Pts = []
-PJ = []
-promArray = []
-prom = []
-
-# team name
-for equipo in eq:
-    equipo = equipo.find_all("td")[1:2]
-    lote = list(map(lambda data: str(data.text), equipo))
-    nameArray.append(lote)
-
-for _ in nameArray:
-    name.append(_[0])
-
-# t20, t21, pts & pj
-for equipo in eq:
-    equipo = equipo.find_all("td")[2:6]
-    lote = list(map(lambda data: int(data.text), equipo))
-    statsArray.append(lote)
-    
-for stat in statsArray:
-    T20.append(stat[0])
-    T21.append(stat[1])
-    Pts.append(stat[2])
-    PJ.append(stat[3])
-        
-# averages  
-for equipo in eq:
-    equipo = equipo.find_all("td")[6:]
-    lote = list(map(lambda data: float(data.text), equipo))
-    promArray.append(lote)
-    
-for _ in promArray:
-    prom.append(_[0])
+# create function
+def extract_table():
+    """Precondition: html previously specified
+       Postcondition: table in list format"""
+    l = []
+    for row in table:
+        team = row.find_all("td")
+        batch = list(map(lambda data: data.text, team))
+        l.append(batch)
+    return l
 
 # create dataframe
-df = pd.DataFrame({'Equipo': pd.Series(name),
-              'T20': pd.Series(T20),
-              'T21': pd.Series(T21),
-              'Pts': pd.Series(Pts),
-              'PJ': pd.Series(PJ),
-              'Prom': pd.Series(prom),
+df = pd.DataFrame(extract_table())
+df = pd.DataFrame({'Equipo': pd.Series(df[1]),
+              'T20': pd.Series(df[2]),
+              'T21': pd.Series(df[3]),
+              'Pts': pd.Series(df[4]),
+              'PJ': pd.Series(df[5]),
+              'Prom': pd.Series(df[6]),
               })
-
 df = df.sort_values(by=["Prom"], ascending = False)
 df = df.reset_index(drop = True)
-    
+
 # save df
-df.to_csv("Promiedos.csv")
-        
-        
-        
+df.to_csv("promiedos.csv")
